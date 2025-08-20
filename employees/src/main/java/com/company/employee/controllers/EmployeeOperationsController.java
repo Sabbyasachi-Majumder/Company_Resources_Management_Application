31250574686859f1b81e9eacbc78091921873d24
Sabbyasachi Majumder <91467554+Sabbyasachi-Majumder@users.noreply.github.com>
2025-08-20 19:10:48 +0530

    basic clean up

--- employees/src/main/java/com/company/employee/controllers/EmployeeOperationsController.java
+++ employees/src/main/java/com/company/employee/controllers/EmployeeOperationsController.java
@@ -15,13 +15,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -56,7 +53,7 @@ public class EmployeeOperationsController {
     @Tag(name = "Health Checks")
     @Operation(summary = "Test database connection", description = "Tests if the connection to the employee database is established.")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Database connection test result", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponseDTO.class)))})
-    public ResponseEntity<ApiResponseDTO<String>> testDataBaseConnection() throws SQLException {
+    public ResponseEntity<ApiResponseDTO<String>> testDataBaseConnection() {
         loggingStart();
         logger.debug("Testing EmployeeOperationsController to employee database connection.");
         return ResponseEntity.ok(employeeService.testDatabaseConnection());
